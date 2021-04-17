b76b303f1a3f20f01ff8b791fa9359a808a651e8
Michael Grimsley <mgrimsley8460@sdsu.edu>
2021-04-16 20:28:18 -0700

    added end game credits

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+    public Button playAgain;
+    public Button quit;
+
+    public void Start() {
+        playAgain = GameObject.Find("PlayAgainButton").GetComponent<Button>();
+        playAgain.onClick.AddListener(PlayAgain);
+
+        quit = GameObject.Find("QuitButton").GetComponent<Button>();
+        quit.onClick.AddListener(Quit);
+    }
+    public void PlayAgain(){
+        GameManager.Reset();
+        SceneManager.LoadScene("Level1");
+    }
+    public void Quit() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
